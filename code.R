ebd36ea3e73d36de4e5f028ef155b69df20e1049
Michael W. Kearney <kearneymw@missouri.edu>
2018-12-21 01:20:27 -0600

    fix mmmm

--- code.R
+++ code.R
@@ -914,15 +914,15 @@ p + ggsave("SVG/h.rtweet.svg", width = (1.73/2) * 8,
 
 
 ##----------------------------------------------------------------------------##
-##                                    mmm                                     ##
+##                                   mmmm                                     ##
 ##----------------------------------------------------------------------------##
 
 
 p <- make_hexsticker(bg = "#3B528BFF", border = "#440154FF", family = "Comfortaa Bold",
-  pkg = "mmm", pkg_size = 42, pkg_color = "#AADC32FF", pkg_y = .05,
+  pkg = "mmmm", pkg_size = 36, pkg_color = "#AADC32FF", pkg_y = .05,
   icon = "", fa = "Font Awesome 5 Brands",
   icon_color = "#AADC32FF", icon_size = 28, icon_y = .295,
-  url = "mmm.mikewk.com", url_color = "#AADC32FF",
+  url = "mmmm.mikewk.com", url_color = "#AADC32FF",
   url_size = 6, url_x = .38, url_y = -.67)
 
 p + ggsave("PNG/mmm.png", width = (1.73/2) * 8,
